1d224674d158976fc28d3b8489f3ae56875bf7b7
S4RMI618 <huessabe@gmail.com>
2023-02-09 12:49:37 -0500

    update

--- cajero.py
+++ cajero.py
@@ -1,4 +1,27 @@
-caja_fuerte = 1000
+from os import system
+system("cls")
+
+b_cien = 5
+b_cincue = 5
+b_veinte = 5
+b_diez = 5
+caja_fuerte = [ b_cien * 100,
+                b_cincue * 50,  
+                b_veinte * 20,
+                b_diez * 10
+                ]
+
+def darbilletes(valor_solicitado,valorDelBillete):
+    contador = 0
+    while valor_solicitado >= valorDelBillete:
+        contador += 1
+        valorDelBillete -= 1
+        valor_solicitado -= valorDelBillete
+        if valorDelBillete == 0:
+            break
+    return valor_solicitado, contador
+
+
 
 while caja_fuerte > 0:
     valor_solicitado = int(input("Ingrese el dinero a retirar: "))
@@ -7,39 +30,22 @@ while caja_fuerte > 0:
             print("Dinero insuficiente en el cajero, por favor utilice otro.")
             break
         else:
-            caja_fuerte - valor_solicitado
-            b_cien = 0
-            b_cincue = 0
-            b_veinte = 0
-            b_diez = 0
-            
-            while valor_solicitado >= 100:
-                b_cien = b_cien +1
-                valor_solicitado -= 100
-                
-            while valor_solicitado >= 50:
-                b_cincue = b_cincue +1
-                valor_solicitado -= 50
-                
-            while valor_solicitado >= 20:
-                b_veinte = b_veinte +1
-                valor_solicitado -= 20
-                
-            while valor_solicitado >= 10:
-                b_diez = b_diez +1
-                valor_solicitado -= 10
-                
-            total = b_cien + b_cincue + b_veinte + b_diez
+            valor_a_restar_en_caja = valor_solicitado
+            valor_solicitado , b_cien   = darbilletes(valor_solicitado,b_cien*100)
+            valor_solicitado , b_cincue = darbilletes(valor_solicitado,b_cincue*50)
+            valor_solicitado , b_veinte = darbilletes(valor_solicitado,b_veinte*20)
+            valor_solicitado , b_diez   = darbilletes(valor_solicitado,b_diez*10)
+            caja_fuerte - valor_a_restar_en_caja
+            DineroRestante = b_cien * 100 + b_cincue * 50 + b_veinte *20 + b_diez * 10
             decision = int(input("¿Desea continuar? 1: Si. 2: No."))
             if decision == 1:
                 print("---------------------------------------------------------------")
-                print("Valor entregado: ", (b_cien*100 + b_cincue*50 + b_veinte*20 + b_diez*10))
+                print("Valor entregado: ", valor_a_restar_en_caja)
                 print("Billetes de 100: ", b_cien)
                 print("Billetes de 50 : ", b_cincue)
                 print("Billetes de 20 : ", b_veinte)
                 print("Billetes de 10 : ", b_diez)
-                print("---------------------------------------------------------------")
-                print("Total billetes : ", total) 
+                print("---------------------------------------------------------------") 
 
             elif decision == 2:
                 print("Transacción cancelada, dinero no entregado.")  
@@ -47,5 +53,5 @@ while caja_fuerte > 0:
                 break         
     else: 
         print("Valor no admitido, denominaciones disponibles: 100, 50, 20, 10 ")
-
+        
 print("Gracias por utilizar nuestros servicios.")
--- test.py
+++ test.py
@@ -2,12 +2,80 @@
 # Denominaciones de los billetes 10 20 50 y 100
 # confirmacion (Seguir o cancelar)
 
-cajita = int(input("valor de cajita: "))
+b_cien = 5
+b_cincue = 5
+b_veinte = 5
+b_diez = 5
+caja_fuerte = b_cien * 100 + b_cincue * 50 + b_veinte * 20 + b_diez * 10
 
-if cajita == 5:
-    print("Efectivamente, es 5")
-elif cajita == 6:
-    print("es 6")
-else:
-    print("usted es tonto")
+while caja_fuerte > 0:
+    valor_solicitado = int(input("Ingrese el dinero a retirar: "))
+    if valor_solicitado % 10 == 0:
+        if valor_solicitado > caja_fuerte:
+            print("Dinero insuficiente en el cajero, por favor utilice otro.")
+            pass
+        else:
+            valor_a_restar_en_caja = valor_solicitado
+            c1 = 0
+            c2 = 0
+            c3 = 0
+            c4 = 0
+            while valor_solicitado >= 100:
+                if b_cien == 0:
+                    break
+                else:
+                    b_cien = b_cien - 1
+                    c1 += 1
+                    valor_solicitado -= 100
+                    caja_fuerte -= 100
+                
 
+            while valor_solicitado >= 50:
+                if b_cincue == 0:
+                    break
+                else: 
+                    b_cincue = b_cincue - 1
+                    c2 += 1
+                    valor_solicitado -= 50
+                    caja_fuerte -= 50
+                
+                
+            while valor_solicitado >= 20:
+                if b_veinte == 0:
+                    break
+                else:
+                    b_veinte = b_veinte - 1
+                    c3 += 1
+                    valor_solicitado -= 20
+                    caja_fuerte -= 20
+                
+            while valor_solicitado >= 10:
+                if b_diez == 0:
+                    break
+                else:
+                    b_diez = b_diez - 1
+                    c4 += 1
+                    valor_solicitado -= 10
+                    caja_fuerte -= 10
+                
+            decision = int(input("¿Desea continuar? 1: Si. 2: No."))
+            if decision == 1:
+                print("---------------------------------------------------------------")
+                print("Valor entregado: ", (c1*100 + c2*50 + c3*20 + c4*10))
+                print("Billetes de 100: ", c1)
+                print("Billetes de 50 : ", c2)
+                print("Billetes de 20 : ", c3)
+                print("Billetes de 10 : ", c4)
+                print("---------------------------------------------------------------")
+                print("Total billetes : ", c1+c2+c3+c4 ) 
+
+            elif decision == 2:
+                print("Transacción cancelada, dinero no entregado.")  
+                print("Cancelando ejecución")
+                pass         
+    else: 
+        print("Valor no admitido, denominaciones disponibles: 100, 50, 20, 10 ")
+        pass
+        
+print("---------------------------------------------------------------")
+print("Cajero sin fondos, vuelva más tarde....")
